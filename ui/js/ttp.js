64edc6bbcb5d3795268ba9e0d32517d4b979f484
Stefan Boila <dumitru.boila@gmail.com>
2016-09-03 17:45:41 +0300

    Integrated the graph to

--- ui/js/ttp.js
+++ ui/js/ttp.js
@@ -13,16 +13,96 @@ var getTimelineData = function () { //input data will be passed as param
     return $('#ttp-events');
 };
 
-var timelineNodeHandler = function () {
+var timelineNodeHandler = function (graph) {
   events.find("li").on("click", function (e) {
       console.log("you clicked " + $(this).attr("id"));
       //todo: using this date get a json with the assocgraph
+      //graph.remove();
+      //console.log("L-am distrus");
+      createGraph();
   });
+};
+
+var createGraph = function () {
+    var cy = cytoscape({
+        container: document.getElementById('daily-graph'),
+
+        boxSelectionEnabled: false,
+        autounselectify: true,
+
+        style: cytoscape.stylesheet()
+            .selector('node')
+            .css({
+                'content': 'data(id)'
+            })
+            .selector('edge')
+            .css({
+                'target-arrow-shape': 'triangle',
+                'width': 4,
+                'line-color': '#ddd',
+                'target-arrow-color': '#ddd',
+                'curve-style': 'bezier'
+            })
+            .selector('.highlighted')
+            .css({
+                'background-color': '#61bffc',
+                'line-color': '#61bffc',
+                'target-arrow-color': '#61bffc',
+                'transition-property': 'background-color, line-color, target-arrow-color',
+                'transition-duration': '0.5s'
+            }),
+
+        elements: {
+            nodes: [
+                { data: { id: 'a' } },
+                { data: { id: 'b' } },
+                { data: { id: 'c' } },
+                { data: { id: 'd' } },
+                { data: { id: 'e' } }
+            ],
+
+            edges: [
+                { data: { id: 'a"e', weight: 1, source: 'a', target: 'e' } },
+                { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
+                { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
+                { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
+                { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
+                { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
+                { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+            ]
+        },
+
+        layout: {
+            name: 'breadthfirst',
+            directed: true,
+            roots: '#a',
+            padding: 10
+        }
+    });
+
+    var bfs = cy.elements().bfs('#a', function(){}, true);
+
+    var i = 0;
+    var highlightNextEle = function(){
+        if( i < bfs.path.length ){
+            bfs.path[i].addClass('highlighted');
+
+            i++;
+            setTimeout(highlightNextEle, 1000);
+        }
+    };
+
+// kick off first highlight
+    highlightNextEle();
+
+    return cy;
 };
 
 jQuery(document).ready(function ($) {
     getTimelineData().promise().done(function () {
         initTimeline($(this));
-        timelineNodeHandler();
+
+        var ttpGraph = createGraph();
+        timelineNodeHandler(ttpGraph);
     });
 });
