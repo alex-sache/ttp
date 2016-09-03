386a6e2c30b710bbe81f23f44a052a21865f6225
Alexandru Sache <alexandru.sache@emag.ro>
2016-09-03 17:40:48 +0300

    test command\

--- tests/AppBundle/Controller/Neo4jControllerTest.php
+++ tests/AppBundle/Controller/Neo4jControllerTest.php
@@ -3,18 +3,11 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use GraphAware\Neo4j\Client\ClientBuilder;
-
 
 class Neo4jControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-
-        $client = ClientBuilder::create()
-            ->addConnection('default', 'http://neo4j:parolaneo4j@46.101.106.91:7474');
-
-        $client->run("CREATE (n:TestPersoana) SET n += {infos}", ['atribute' => ['name' => 'Alex', 'age' => 35]]);
 
     }
 }
