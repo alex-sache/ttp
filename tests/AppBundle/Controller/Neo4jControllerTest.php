417390898a0461bac1388fa758fd0ac675b3b97a
Alexandru Sache <alexandru.sache@emag.ro>
2016-09-03 17:07:31 +0300

    neo test

--- tests/AppBundle/Controller/Neo4jControllerTest.php
+++ tests/AppBundle/Controller/Neo4jControllerTest.php
@@ -3,16 +3,18 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use GraphAware\Neo4j\Client\ClientBuilder;
+
 
 class Neo4jControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $client = ClientBuilder::create()
+            ->addConnection('default', 'http://neo4j:parolaneo4j@46.101.106.91:7474');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $client->run("CREATE (n:TestPersoana) SET n += {infos}", ['atribute' => ['name' => 'Alex', 'age' => 35]]);
+
     }
 }
