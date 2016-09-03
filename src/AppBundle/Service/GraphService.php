3fa55ae26c017dcd7b9cf327ff204b6c425155e5
Oana Buga <oana.buga@emag.ro>
2016-09-03 22:39:55 +0300

    create relationship method

--- src/AppBundle/Service/GraphService.php
+++ src/AppBundle/Service/GraphService.php
@@ -27,10 +27,41 @@ class GraphService
     public function createNode($type, $labels = [])
     {
         $client = $this->buildClient();
+
         $query = "CREATE (n:$type) SET n += {infos}";
         $parameters = ['infos' => $labels];
-
         $client->run($query, $parameters);
+
+        return true;
+    }
+
+    /**
+     * @param array $nodeSource
+     * @param array $nodeDestination
+     * @param $relType
+     * @return bool
+     * @throws \GraphAware\Neo4j\Client\Exception\Neo4jException
+     */
+    public function createRelationship($nodeSource, $nodeDestination, $relType)
+    {
+        $client = $this->buildClient();
+
+        $nodeSourceType = $nodeSource['type'];
+        $nodeSourceLabelKey = $nodeSource['labelKey'];
+        $nodeSourceLabelValue = $nodeSource['labelValue'];
+
+        $nodeDestinationType = $nodeDestination['type'];
+        $nodeDestinationLabelKey = $nodeDestination['labelKey'];
+        $nodeDestinationLabelValue = $nodeDestination['labelValue'];
+
+        $query = "MATCH (a:$nodeSourceType),(b:$nodeDestinationType)
+            WHERE a.$nodeSourceLabelKey = '$nodeSourceLabelValue'
+            AND b.$nodeDestinationLabelKey = '$nodeDestinationLabelValue'
+            CREATE (a)-[r:$relType]->(b)
+            RETURN r
+        ";
+
+        $client->run($query);
 
         return true;
     }
