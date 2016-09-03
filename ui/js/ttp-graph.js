ae1773ba8fefe757df250ea69c8b1bb06b66a887
Stefan Boila <dumitru.boila@gmail.com>
2016-09-03 13:17:40 +0300

    Added a demo structure

--- ui/js/ttp-graph.js
+++ ui/js/ttp-graph.js
@@ -10,7 +10,7 @@ $(function(){ // on dom ready
             .selector('node')
             .css({
                 'shape': 'data(faveShape)',
-                'width': 'mapData(weight, 40, 80, 20, 60)',
+                'width': 'mapData(weight, 30, 30, 20, 20)',
                 'content': 'data(name)',
                 'text-valign': 'center',
                 'text-outline-width': 2,
@@ -47,24 +47,26 @@ $(function(){ // on dom ready
 
         elements: {
             nodes: [
-                { data: { id: 'j', name: 'Jerry', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } },
-                { data: { id: 'e', name: 'Elaine', weight: 45, faveColor: '#EDA1ED', faveShape: 'ellipse' } },
-                { data: { id: 'k', name: 'Kramer', weight: 75, faveColor: '#86B342', faveShape: 'octagon' } },
-                { data: { id: 'g', name: 'George', weight: 70, faveColor: '#F5A45D', faveShape: 'rectangle' } }
+                { data: { id: 'j', name: 'Oana', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } },
+                { data: { id: 'e', name: 'Alex', weight: 45, faveColor: '#EDA1ED', faveShape: 'ellipse' } },
+                { data: { id: 'k', name: 'Laurentiu', weight: 75, faveColor: '#86B342', faveShape: 'octagon' } },
+                { data: { id: 'p', name: 'Adrian', weight: 75, faveColor: '#86B342', faveShape: 'rectangle' } },
+                { data: { id: 'g', name: 'Stefan', weight: 70, faveColor: '#F5A45D', faveShape: 'rectangle' } }
             ],
             edges: [
-                { data: { source: 'j', target: 'e', faveColor: '#6FB1FC', strength: 90 } },
-                { data: { source: 'j', target: 'k', faveColor: '#6FB1FC', strength: 70 } },
-                { data: { source: 'j', target: 'g', faveColor: '#6FB1FC', strength: 80 } },
+                { data: { source: 'j', target: 'e', faveColor: '#6FB1FC', strength: 20 } },
+                { data: { source: 'j', target: 'k', faveColor: '#6FB1FC', strength: 20 } },
+                { data: { source: 'j', target: 'g', faveColor: '#6FB1FC', strength: 20 } },
 
-                { data: { source: 'e', target: 'j', faveColor: '#EDA1ED', strength: 95 } },
-                { data: { source: 'e', target: 'k', faveColor: '#EDA1ED', strength: 60 }, classes: 'questionable' },
+                { data: { source: 'e', target: 'j', faveColor: '#EDA1ED', strength: 30 } },
+                { data: { source: 'e', target: 'k', faveColor: '#EDA1ED', strength: 30 }, classes: 'questionable' },
 
-                { data: { source: 'k', target: 'j', faveColor: '#86B342', strength: 100 } },
-                { data: { source: 'k', target: 'e', faveColor: '#86B342', strength: 100 } },
-                { data: { source: 'k', target: 'g', faveColor: '#86B342', strength: 100 } },
+                { data: { source: 'k', target: 'j', faveColor: '#86B342', strength: 30 } },
+                { data: { source: 'k', target: 'e', faveColor: '#86B342', strength: 30 } },
+                { data: { source: 'k', target: 'g', faveColor: '#86B342', strength: 30 } },
 
-                { data: { source: 'g', target: 'j', faveColor: '#F5A45D', strength: 90 } }
+                { data: { source: 'g', target: 'j', faveColor: '#F5A45D', strength: 4 } },
+                { data: { source: 'e', target: 'p', faveColor: '#F5A45D', strength: 20 } }
             ]
         },
 
