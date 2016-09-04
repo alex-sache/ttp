80db0a2ffc7dfcac77b9f1cf3d518181485a3a71
Oana Buga <oana.buga@emag.ro>
2016-09-04 09:32:37 +0300

    search in by nodes and relationships

--- src/AppBundle/Service/GraphService.php
+++ src/AppBundle/Service/GraphService.php
@@ -65,4 +65,69 @@ class GraphService
 
         return true;
     }
+
+    public function searchIn($queryParams = [])
+    {
+        $client = $this->buildClient();
+
+        $nodeSource = $queryParams['nodeSource'];
+        $nodeSourceType = $nodeSource['type'];
+        $nodeSourceLabelKey = $nodeSource['labelKey'];
+        $nodeSourceLabelValue = $nodeSource['labelValue'];
+
+        $nodeDestination = [];
+        if (isset($queryParams['nodeDestination'])) {
+            $nodeDestination = $queryParams['nodeDestination'];
+            $nodeDestinationType = $nodeDestination['type'];
+            $nodeDestinationLabelKey = $nodeDestination['labelKey'];
+            $nodeDestinationLabelValue = $nodeDestination['labelValue'];
+        }
+
+        $lastNode = [];
+        if (isset($queryParams['lastNode'])) {
+            $lastNode = $queryParams['lastNode'];
+            $lastNodeType = $lastNode['type'];
+            $lastNodeLabelKey = $lastNode['labelKey'];
+            $lastNodeLabelValue = $lastNode['labelValue'];
+        }
+
+
+        $firstRelType = $queryParams['firstRelType'];
+        $lastRelType = [];
+        if (isset($queryParams['lastRelType'])) {
+            $lastRelType = $queryParams['lastRelType'];
+        }
+
+        $query = "MATCH (a:$nodeSourceType)";
+
+        if (!empty($firstRelType) && empty($nodeDestination)) {
+            $query .= "-[r:$firstRelType]->()";
+        } elseif (!empty($firstRelType) && !empty($nodeDestination)) {
+            $query .= "-[r:$firstRelType]->(b:$nodeDestinationType)";
+        }
+
+
+        if (!empty($lastRelType) && empty($lastNode)) {
+            $query .= "-[r:$lastRelType]->()";
+        } elseif (!empty($lastRelType) && !empty($lastNode)) {
+            $query .= "-[r:$lastRelType]->(c:$lastNodeType)";
+        }
+
+        $query .= " WHERE a.$nodeSourceLabelKey = '$nodeSourceLabelValue'";
+
+        if(!empty($nodeDestination)) {
+            $query .= "AND b.$nodeDestinationLabelKey = '$nodeDestinationLabelValue'";
+        }
+
+        if(!empty($lastNode)) {
+            $query .= "AND c.$lastNodeLabelKey = '$lastNodeLabelValue'";
+        }
+
+        $query .= " RETURN *";
+        $query .= " LIMIT 5";
+
+        $result = $client->run($query);
+
+        return $result;
+    }
 }
